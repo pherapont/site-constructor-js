6291fbdcfcb9301e17de5b39946050a510f42d89
pherapont <pherapont@yandex.ru>
2020-09-22 15:12:26 +0700

    Inttial commit

---
+++ src/model.js
@@ -0,0 +1,12 @@
+export const model = [
+	{type: 'title', value:'Конструктор сайтов', options: {}},
+	{type: 'text', value:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit ex, numquam sint alias asperiores voluptates similique! Laboriosam omnis fugiat aperiam! Fugit qui maxime exercitationem quos impedit provident nesciunt iure accusantium!', options: {}},
+	{type: 'textColumns', value: [
+		'Текст 1',
+		'Текст 2',
+		'Текст 3',
+		'Текст 4',
+	], options: {}},
+	{type: 'title', value:'Конструктор сайтов', options: {}},
+
+]
